409c90f727c2ee0a7b0507ab6f3979e0efa6d154
천명현 <cmh1448@naver.com>
2025-06-03 17:26:13 +0900

    feat: setup.sh 스크립트 개선 및 모델 다운로드 추가
    - 가상 환경 설정 및 활성화 추가
    - torch 설치 명령어 추가
    - 모델 다운로드 명령어 주석 제거 및 위치 조정
    - start.sh 실행 및 로그 출력 추가

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-# download the model to ./models/model.safetensors
-# download the model from https://huggingface.co/Lykon/dreamshaper-xl-v2-turbo/resolve/main/DreamShaperXL_Turbo_V2-SFW.safetensors?download=true
+#!/bin/bash
 
-# install the dependencies
+python -m venv venv
+source venv/bin/activate
+
+# install torch 2.7.0+cu124
+pip install torch --index-url https://download.pytorch.org/whl/cu124
 pip install -r requirements.txt
 
 # download the model
 mkdir -p models
 curl -L https://huggingface.co/Lykon/dreamshaper-xl-v2-turbo/resolve/main/DreamShaperXL_Turbo_V2-SFW.safetensors?download=true -o models/model.safetensors
+
+./start.sh
+tail -f app.log
